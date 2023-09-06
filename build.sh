ddd6cda2a7258b565086325c076dabca1e6824cb
shawfeng <563766395@qq.com>
2023-09-06 10:02:45 +0000

    complete autobuild

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ SOURCE_DIR=$(pwd)
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 
-mkdir -p ${BUILD_DIR}/${BUILD_TYPE}\
+mkdir -p ${BUILD_DIR}/${BUILD_TYPE} \
   && cd ${BUILD_DIR}/${BUILD_TYPE} \
   && cmake \
        -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
        ${SOURCE_DIR} \
-  && make
+  && make \
+  && mv HttpServer ${SOURCE_DIR}
